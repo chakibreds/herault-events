3ef0bb3b80cf097b7a0c508bfb3763752241bd1f
chakib <celhouiti@gmail.com>
2019-10-28 13:20:33 +0100

    debut du projet avec la page principale du site

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HéraultEvents</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header class="container-fluid">
+        <nav class="row" class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="col-lg-offset-10"></div>
+            <button class="btn btn-outline-success my-1 my-sm-0" type="submit">Sign in</button>
+            <button class="btn btn-outline-success my-1 my-sm-0" type="submit">Sign up</button>
+        </nav>
+
+    </header>
+</body>
+
+</html>
