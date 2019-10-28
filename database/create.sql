480dce04b4fa2787774c3f94dfb689bdc0c34e15
Massili Kezzoul <massy.kezzoul@gmail.com>
2019-10-28 13:20:39 +0100

    Init create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,18 @@
 /* 
     Ce fichier contient la définition des tables utilisé dans la projet herault-events
 */
+
+/* 
+    Suppression des tables si ils existes
+*/
+
+DELETE TABLE IF EXISTS user;
+DELETE TABLE IF EXISTS adress;
+DELETE TABLE IF EXISTS events;
+DELETE TABLE IF EXISTS participate;
+DELETE TABLE IF EXISTS interested;
+DELETE TABLE IF EXISTS comments;
+
+/* 
+    Création des tables
+*/
