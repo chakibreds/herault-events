8d0b27a9335beff01ab4189081f41bd4ee00f77f
Massili Kezzoul <massy.kezzoul@gmail.com>
2019-10-29 11:48:05 +0100

    add table user on create.sql

--- database/create.sql
+++ database/create.sql
@@ -6,13 +6,34 @@
     Suppression des tables si ils existes
 */
 
-DELETE TABLE IF EXISTS user;
-DELETE TABLE IF EXISTS adress;
-DELETE TABLE IF EXISTS events;
-DELETE TABLE IF EXISTS participate;
-DELETE TABLE IF EXISTS interested;
-DELETE TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS adress;
+DROP TABLE IF EXISTS participate;
+DROP TABLE IF EXISTS interested;
+DROP TABLE IF EXISTS comment;
 
 /* 
     Création des tables
 */
+/* Table adress*/
+CREATE TABLE adress (
+    id_address INT AUTO_INCREMENT, 
+)
+
+/* Table user */
+CREATE TABLE user (
+    user_name VARCHAR(100),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birthday DATE NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(20) UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    registration_date DATETIME NOT NULL,
+    role ENUM('vistor','contributor','admin') NOT NULL DEFAULT 'visitor',
+    id_address INT,
+    PRIMARY KEY (user_name),
+    FOREIGN KEY (id_address) REFERENCES adress(id_address)
+
+);
