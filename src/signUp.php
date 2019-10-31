8eceac2a981ba5b95005d66b1a08cec36431f280
chakib <celhouiti@gmail.com>
2019-10-31 21:57:16 +0100

    page de connexion

---
+++ src/signUp.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Join HéraultEvents</title>
+</head>
+
+<body>
+    <header class="">
+        <span class="logo">
+            <h1>LOGO</h1>
+        </span>
+        
+        <form action="" class="sign-in">
+            <div class="pseudo">
+                <label for="pseudo">Nom d'utilisateur</label>
+                <input type="text" name="pseudo" id="pseudo">
+            </div>
+            <div class="nom">
+                <label for="nom">Nom </label>
+                <input type="text" name="nom" id="nom">
+            </div>
+            <div class="prenom">
+                <label for="prenom">Prénom</label>
+                <input type="text" name="prenom" id="prenom">
+            </div>
+            <div class="date_nai">
+                <label for="date_nai">Date de naissance</label>
+                <input type="date" name="date_nai" id="date_nai">
+            </div>
+            <div class="email">
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div class="tel">
+                <label for="tel">Téléphone</label>
+                <input type="tel" name="tel" id="tel">
+            </div>
+            <div class="password">
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <button type="submit" class="btn">Sign Up</button>
+        </form>
+
+    </header>
+</body>
+
+</html>
