2b1d779f0b3414348860c88c35316684623a3861
chakib <celhouiti@gmail.com>
2019-11-26 17:11:42 +0100

    regroupement des fichiers sass

--- view/head.php
+++ view/head.php
@@ -4,7 +4,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HéraultEvents</title>
     <link rel="stylesheet" href="<?= $server_root ?>view/css/style.css">
-    <link rel="stylesheet" href="<?= $server_root ?>view/css/media.css">
     <link rel="icon" href="<?= $server_root ?>view/img/logo/HE-icon-tiny.ico" type="image/ico">
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/80ea9751af.js" crossorigin="anonymous"></script>
