f2db9c21326955f3daf17629956fe3bc5568862d
chakib <celhouiti@gmail.com>
2019-11-27 14:16:57 +0100

    merge

--- view/head.php
+++ view/head.php
@@ -7,5 +7,5 @@
     <link rel="icon" href="<?= $server_root ?>view/img/logo/HE-icon-tiny.ico" type="image/ico">
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/80ea9751af.js" crossorigin="anonymous"></script>
-    <script src="<?= $server_root ?>view/js/script.js"></script>
+   
 </head>
