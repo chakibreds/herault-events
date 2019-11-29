1043ba0e5161071651af3761e1c0ec5085b47627
chakib <celhouiti@gmail.com>
2019-11-29 12:02:21 +0100

    script inscription marche bien

--- view/js/inscription.js
+++ view/js/inscription.js
@@ -2,47 +2,70 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var form = document.querySelector(".inscription form");
     var inscrire = form.querySelector("button.inscrire");
     inscrire.style.display = "none";
-    form.querySelector(".donnees-adresse").style.display = "none";
-    form.querySelector(".donnees-connection").style.display = "none";
+
     var next = form.querySelector("button.next");
     var retour = form.querySelector("button.retour");
     retour.style.display = "none";
     var nbNext = 1;
-    var perso =  form.querySelector(".donnees-personnels");
+    var perso = form.querySelector(".donnees-personnels");
     var connection = form.querySelector(".donnees-connection");
     var adresse = form.querySelector(".donnees-adresse");
+
+    adresse.style.display = "none";
+    connection.style.display = "none";
+    //next.disabled = true;
+    var inputPerso = perso.querySelectorAll("input");
+    /* let nChange = 0;
+    for (const input of inputPerso) {
+        input.addEventListener("change", function () {
+            nChange++;
+            console.log(nChange);
+            if (nChange == inputPerso.length) {
+                next.disabled = false;
+            }
+        });
+
+    } */
+
+
+
 
     next.addEventListener("click", function () {
         /* if (nbNext == 0) {
            
             nbNext++;
         } */
+
        
-        if (nbNext == 1) {
-            inscrire.style.display = "none";
-            next.style.display = "block";
-            perso.style.display = "none";
-            connection.style.display = "none";
-            adresse.style.display = "block";
-            retour.style.display = "block";
-            nbNext++;
-            return;
-        }
-        if (nbNext == 2) {
-            inscrire.style.display = "block";
-            perso.style.display = "none";
-            connection.style.display = "block";
-            adresse.style.display = "none";
-            next.style.display = "none";
-            retour.style.display = "block";
-            nbNext++;
-            return;
-        }
-        if (nbNext == 3) {
-            console.log("fini l'inscription");
-            nbNext = 0;
-            return;
-        }
+
+            if (nbNext == 1) {
+                if (perso.querySelector("#nom").value != "" && perso.querySelector("#prenom").value != "" && perso.querySelector("#date_nai").value != "") {
+
+                inscrire.style.display = "none";
+                next.style.display = "block";
+                perso.style.display = "none";
+                connection.style.display = "none";
+                adresse.style.display = "block";
+                retour.style.display = "block";
+                nbNext++
+                return;
+            }
+            }
+            if (nbNext == 2) {
+                inscrire.style.display = "block";
+                perso.style.display = "none";
+                connection.style.display = "block";
+                adresse.style.display = "none";
+                next.style.display = "none";
+                retour.style.display = "block";
+                nbNext++;
+                return;
+            }
+            if (nbNext == 3) {
+                console.log("fini l'inscription");
+                nbNext = 0;
+                return;
+            }
     });
     retour.addEventListener("click", function () {
         if (nbNext == 2) {
