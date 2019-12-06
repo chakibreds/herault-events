e1fe97e251acadf43b9a255d80684a649a8f0762
chakib <celhouiti@gmail.com>
2019-12-06 01:15:04 +0100

    ça marche enfin

--- model/model.php
+++ model/model.php
@@ -1,10 +1,12 @@
 <?php
+/* pour que la connection marche il faut changer la variable MysqlUserName avec le nom d'utilasateur de votre mysql */
 
+require_once $dir_root . 'model/mysqlUserName.php';
 class Model
 {
     protected static function dbConnect()
     {
-        $user_name = "massyUniv";
+        $user_name = variable::$mysql_user_name;
         $password = "password";
         $db_name = "herault_events";
         $db = new PDO("mysql:host=localhost;dbname=$db_name;charset=utf8", $user_name, $password);
