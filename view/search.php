fe6ad8a58cb3e1070f5a64fe866e62fe0e156c2c
chakib <celhouiti@gmail.com>
2019-12-10 23:42:05 +0100

    un petit avancement dans la page search

--- view/search.php
+++ view/search.php
@@ -37,18 +37,74 @@ if (isset($_SESSION['user']) && logged($_SESSION['user'])) {
             </ul>
         </aside>
         <article class="search">
-            <section class="criteres"></section>
+            
+           
+            <section class="criteres-textuelles">
             <div class="label-input">
-                <label for="date-event">Par date </label>
+                <label for="titre">Par titre</label>
+                <input type="text" name="titre" id="titre">
+            </div>
+            
+            <div class="label-input">
+                <label for="ville">Par ville</label>
+                <input type="text" name="ville" id="ville">
+            </div>
+            </section>
+            <button class = "search" type="button">search <i class="fas fa-search"></i></button>
+            <section class="criteres-choix">
+            <div class="label-input">
+                <label for="date-event">Par date</label>
                 <select name="date-event" id="date-event">
                     <option value="asc">Du plus récent au plus ancien </option>
                     <option value="desc">Du plus ancien au plus récent </option>
                 </select>
             </div>
             <div class="label-input">
-                <label for="ville">Par ville</label>
-                <input type="text" name="ville" id="ville">
+                <label for="note-event">Par note </label>
+                <select name="note-event" id="note-event">
+                    <option value="asc">Ordre croissant</option>
+                    <option value="desc">Ordre décroissant </option>
+                </select>
             </div>
+            </section>
+            <section class="events">
+            <h2>Resultats de la recherche</h2>
+                <ul>
+                    <a href="<?= $server_root ?>view/events.php?event=2" class="event-card">
+                        <div class=image>
+                            <img src="<?= $server_root ?>view/img/compressed/background-comedie-1.jpg">
+                        </div>
+                        <h3>Title event</h3>
+                        <div class="localisation">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <p>Location</p>
+                        </div>
+                        <button type="button"><i class="fas fa-minus"></i> Supprimer</button>
+                    </a>
+                    <a href="<?= $server_root ?>view/events.php?event=2" class="event-card">
+                        <h3>Title event</h3>
+                        <div class=image>
+                            <img src="<?= $server_root ?>view/img/compressed/background-comedie-1.jpg">
+                        </div>
+                        <div class="localisation">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <p>Location</p>
+                        </div>
+                        <button type="button"><i class="fas fa-minus"></i> Supprimer</button>
+                    </a>
+                    <a href="<?= $server_root ?>view/events.php?event=2" class="event-card">
+                        <h3>Title event</h3>
+                        <div class=image>
+                            <img src="<?= $server_root ?>view/img/compressed/background-comedie-1.jpg">
+                        </div>
+                        <div class="localisation">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <p>Location</p>
+                        </div>
+                        <button type="button"><i class="fas fa-minus"></i> Supprimer</button>
+                    </a>
+                    </ul>
+            </section>
         </article>
     </main>
 </body>
